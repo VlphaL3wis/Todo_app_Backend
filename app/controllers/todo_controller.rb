883abb5e7a2aad75d2d1b0ad043b72b6fe21dd75
Mykal Lewis <mykal.lewis@epicacademy.org>
2017-10-22 19:18:19 +0000

    changing routes

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,15 +1,31 @@
 class TodoController < ApplicationController
     def index
+        @todos=Todo.all
     end
+    
     def new 
     end
-        def create
-  t = Todo.new
-  t.description = params['description']
-  t.pomodoro_estimate = params['pomodoro_estimate']
-  t.save
-  redirect_to "/todo/show/#{ t.id }"
-        end
+    
+    def create
+        t = Todo.new
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro_estimate']
+        t.save
+        redirect_to "/todo/show/#{ t.id }"
+    end
+        
+    def edit
+        @todo= Todo.find_by_id(params[:id])
+    end
+            
+    def update
+        t = Todo.find_by_id(params['id'])
+        t.description = params['description']
+        t.pomodoro_estimate = params['pomodoro-estimate']
+        t.save
+        redirect_to "/todo/show/{t.id}"
+    end
+    
     def show
         @todo = Todo.find_by_id(params[:id])
     end
